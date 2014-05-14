f7e73dd2ff916b6463e46e599eb90e158932f8cc
Danielle Madeley <danielle@madeley.id.au>
2014-05-14 13:17:17 +1000

    Add a group of dice

--- dice.py
+++ dice.py
@@ -1,8 +1,12 @@
+from copy import copy
+from functools import reduce
+
 import numpy as np
 
 
 class Die(object):
     def __init__(self, sides):
+        self.multiplier = 1
         self.sides = sides
 
     def __repr__(self):
@@ -13,6 +17,12 @@ class Die(object):
 
         return Dice(other, self.sides)
 
+    def __add__(self, other):
+        if isinstance(other, Die) and self.sides == other.sides:
+            return Dice(self.multiplier + other.multiplier, self.sides)
+        else:
+            return DiceGroup() + self + other
+
     def probability(self):
         scores = np.array(range(1, self.sides + 1))
         onedice = totaldice = np.ones(self.sides) / self.sides
@@ -20,7 +30,7 @@ class Die(object):
         return (scores, onedice)
 
 
-class Dice(object):
+class Dice(Die):
     def __init__(self, multiplier, sides):
         self.multiplier = multiplier
         self.sides = sides
@@ -37,3 +47,50 @@ class Dice(object):
             totaldice = np.convolve(totaldice, onedice)
 
         return (scores, totaldice)
+
+
+class DiceGroup(object):
+    def __init__(self):
+        self.dice = {}
+        self.static = 0
+
+    def __repr__(self):
+        string = ' + '.join(map(str, self.dice.values()))
+
+        if self.static > 0:
+            string += ' + %d' % self.static
+        elif self.static < 0:
+            string += ' - %d' % self.static
+
+        return string
+
+    def __add__(self, other):
+        self = copy(self)
+
+        if isinstance(other, DiceGroup):
+            for dice in other.dice.values():
+                self += dice
+        elif isinstance(other, Die):
+            try:
+                self.dice[other.sides] += other
+            except KeyError:
+                self.dice[other.sides] = other
+        elif isinstance(other, int):
+            self.static += other
+        else:
+            raise NotImplementedError("Unknown type to add")
+
+        return self
+
+    def probability(self):
+
+        scores, probabilities = zip(*(dice.probability()
+                                    for dice in self.dice.values()))
+
+        lowest_score = sum(score[0] for score in scores)
+        highest_score = sum(score[-1] for score in scores)
+        scores = np.array(range(lowest_score, highest_score + 1)) + self.static
+
+        probabilities = reduce(np.convolve, probabilities)
+
+        return (scores, probabilities)
--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
-from pyparsing import CaselessLiteral, Optional, Word
+import operator
+from functools import reduce
 
-from dice import Dice, Die
+from pyparsing import CaselessLiteral, Optional, Word, ZeroOrMore, Literal
+
+from dice import Die
 
 NUMBER = Word('0123456789')
 NUMBER.setParseAction(lambda tokens: int(tokens[0]))
@@ -11,8 +14,12 @@ DIE.setParseAction(lambda tokens: Die(tokens.sides))
 DICE = NUMBER('multiplier') + DIE('die')
 DICE.setParseAction(lambda tokens: tokens.die * tokens.multiplier)
 
-PARSER = DIE | DICE
+
+DICE_GROUP = \
+    (DIE | DICE | NUMBER) + \
+    ZeroOrMore(Literal('+').suppress() + (DIE | DICE | NUMBER))
+DICE_GROUP.setParseAction(lambda tokens: reduce(operator.add, tokens))
 
 
 def parse(string):
-    return PARSER.parseString(string)[0]
+    return DICE_GROUP.parseString(string)[0]
